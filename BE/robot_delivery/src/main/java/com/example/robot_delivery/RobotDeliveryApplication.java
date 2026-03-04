dd4132fdf4d2eea4564baa7e6e700da40b8cc067
spad0604 <giap.nv225126@sis.hust.edu.vn>
2026-03-04 07:21:57 +0700

    FEATURE: Update BE base

---
+++ BE/robot_delivery/src/main/java/com/example/robot_delivery/RobotDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.robot_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotDeliveryApplication.class, args);
+	}
+
+}
