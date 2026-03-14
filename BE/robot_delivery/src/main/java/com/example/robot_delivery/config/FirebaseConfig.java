20a59e45a549b13a0f2a062d96456c6c5609e17a
spad0604 <giap.nv225126@sis.hust.edu.vn>
2026-03-14 17:28:43 +0700

    FEATURE: Update BE notification

--- BE/robot_delivery/src/main/java/com/example/robot_delivery/config/FirebaseConfig.java
+++ BE/robot_delivery/src/main/java/com/example/robot_delivery/config/FirebaseConfig.java
@@ -1,4 +1,35 @@
 package com.example.robot_delivery.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
 public class FirebaseConfig {
+    @PostConstruct
+    public void initialize() {
+        try {
+            InputStream serviceAccount =
+                    getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+
+            if (serviceAccount == null) {
+                throw new RuntimeException("Không tìm thấy file serviceAccountKey.json");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+            System.out.println("Firebase đã được khởi tạo thành công!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- BE/robot_delivery/src/main/java/com/example/robot_delivery/service/NotificationService.java
+++ BE/robot_delivery/src/main/java/com/example/robot_delivery/service/NotificationService.java
@@ -1,4 +1,19 @@
 package com.example.robot_delivery.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
 public class NotificationService {
+    public String sendNotification(String token, String title, String body) throws Exception {
+        Message message = Message.builder()
+                .setToken(token)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+
+        return FirebaseMessaging.getInstance().send(message);
+    }
 }
